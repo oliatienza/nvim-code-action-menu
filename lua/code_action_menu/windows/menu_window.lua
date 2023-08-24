4c937b549d9129279c84e79addce1920575938f8
Oliver Atienza <oliatienza@gmail.com>
2023-08-24 15:54:04 +0800

    fix: Add a bullet at the start of an action item

--- lua/code_action_menu/windows/menu_window.lua
+++ lua/code_action_menu/windows/menu_window.lua
@@ -19,7 +19,7 @@ local function format_summary_for_action(action, index)
   local kind = format_action_kind(action:get_kind())
   local title = action:get_title()
   local disabled = action:is_disabled() and ' [disabled]' or ''
-  return kind .. title .. disabled .. formatted_index
+  return '• ' .. kind .. title .. disabled .. formatted_index
 end
 
 local MenuWindow = StackingWindow:new()
